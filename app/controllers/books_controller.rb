b771114c30e72667f0905082518d47f7cab00640
akane0915 <asialkane@gmail.com>
2017-07-22 09:45:39 -0700

    add index, show, create methods to book and chapter controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,25 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+  end
+
+private
+  def book_params
+    params.require(:book).permit(
+      :title,
+      :landscape_type,
+      :family_type,
+      :food,
+      :girl_name,
+      :house_type,
+      :food_description_one,
+      :food_description_two,
+      :seat_type,
+      :bed_type
+    )
+  end
 end
--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,10 +1,27 @@
 class ChaptersController < ApplicationController
   def index
+    @book = Book.find(params[:book_id])
     @chapters = Chapter.all
   end
 
   def show
+    @book = Book.find(params[:book_id])
     @chapter = Chapter.find(params[:id])
+  end
+
+  def create
+    book = Book.find(params[:book_id])
+    chapter = book.chapters.new(chapter_params)
+    chapter.save
+  end
+
+private
+  def chapter_params
+    params.require(:chapter).permit(
+      :number,
+      :title,
+      :text,
+    )
   end
 
 end
