d146a16bfc847642a3be269a852d89d0fe81dd24
Asia Kane <asialkane@gmail.com>
2017-07-24 11:27:42 -0700

    change landscape form from text field to drop down and clean up whitespace in files

--- app/assets/javascripts/components/chapter1.jsx
+++ app/assets/javascripts/components/chapter1.jsx
@@ -8,29 +8,18 @@ var Chapter1 = React.createClass({
 
   updateLandscapeSubmit: function(e){
     e.preventDefault();
-
-    console.log("Landscape type updated to: " + this.state.landscape_type);
-
     $.ajax({
       url: "/books/1",
       dataType: 'json',
       type: 'PATCH',
       data: {book: {landscape_type: this.state.landscape_type}},
-
       success: function(book) {
-
-       this.props.parentUpdateBook(book);
-
+        this.props.parentUpdateBook(book);
       }.bind(this),
-
       error: function(response, status, err) {
-
-       console.log("An error occurred");
+        console.log("An error occurred");
       }
     });
-
-
-
   },
 
   handleLandscapeChange: function(e){
@@ -44,18 +33,19 @@ var Chapter1 = React.createClass({
           <h3>{this.props.chapter.title}</h3>
 
           <form onSubmit={this.updateLandscapeSubmit}>
-            <input
-              name="book[landscape_type]"
-              type="string"
-              placeholder="Landscape type"
-              value={this.state.landscape_type}
-              onChange={this.handleLandscapeChange}
-              className="string form-control"
-            />
+            <select name="book[landscape_type]" value={this.state.landscape_type}
+            onChange={this.handleLandscapeChange}>
+
+              <option value=""></option>
+              <option value="forest">Forest</option>
+              <option value="tundra">Tundra</option>
+              <option value="grassland">Grassland</option>
+              <option value="desert">Desert</option>
+              <option value="ocean">Ocean</option>
+            </select>
 
             <input type="submit" value="Save" className='btn btn-primary' />
           </form>
-
 
           <p>
             Once upon a time, deep in a {this.state.landscape_type} lived a {this.props.book.family_type} family.  There was a great  big Papa {this.props.book.family_type}, a medium sized Mama {this.props.book.family_type}, and a wee little Baby {this.props.book.family_type}.
