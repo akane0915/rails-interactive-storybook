8549a639be8249e41212cae0595c3bf070ca1300
Asia Kane <asialkane@gmail.com>
2017-07-24 10:06:44 -0700

    add parentBookSubmit function to chapter jsx

--- app/assets/javascripts/components/chapter.jsx
+++ app/assets/javascripts/components/chapter.jsx
@@ -3,15 +3,41 @@ var Chapter = React.createClass({
   getInitialState: function(){
     return {
       book: this.props.book,
-      chapter: this.props.chapter,
     }
+  },
+
+  parentBookSubmit: function(formData){
+
+    $.ajax({
+     url: "/books/1/chapters/1",
+     dataType: 'json',
+     type: 'PATCH',
+     data: formData,
+
+     success: function(book) {
+
+       this.setState({book: book});
+
+     }.bind(this),
+
+     error: function(response, status, err) {
+
+       console.log("An error occurred")
+     }
+
+
+    });
   },
 
   render: function() {
     if (this.props.chapter.number == 1) {
       return (
         <div>
-          <Chapter1 chapter = {this.state.chapter} book = {this.state.book}/>
+          <Chapter1
+            chapter = {this.state.chapter}
+            book = {this.state.book
+            parentBookSubmit={this.parentBookSubmit
+          }/>
         </div>
       )
     } else if (this.props.chapter.number == 2) {
