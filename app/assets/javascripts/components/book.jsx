d1533a8b721dcbcb51782e2aba12daadf7cbccc7
Guest User <Guest@Epicodus-1C.local>
2017-07-24 09:03:37 -0700

    make book title a link to chapter 1 from book index

--- app/assets/javascripts/components/book.jsx
+++ app/assets/javascripts/components/book.jsx
@@ -2,7 +2,9 @@ var Book = React.createClass({
   render: function() {
     return (
       <div>
-        <h3>{this.props.book.title}</h3>
+        <a href={"books/" + this.props.book.id + "/chapters/1"}>
+          Goldilocks and the Three Bears...or Something Like That
+        </a>
       </div>
     )
   }
