38d1cee59cbe12b0f5e5d6e275c395088c9eecda
Asia Kane <asialkane@gmail.com>
2017-07-26 11:38:34 -0700

    add styling to homepage

--- app/assets/javascripts/components/books/book.jsx
+++ app/assets/javascripts/components/books/book.jsx
@@ -2,9 +2,11 @@ var Book = React.createClass({
   render: function() {
     return (
       <div>
-        <a href={"books/" + this.props.book.id + "/chapters/1"}>
-          Goldilocks and the Three Bears...or Something Like That
-        </a>
+        <div className="book_title animated slideInLeft">
+          <a id= "book1_title_link"  href={"books/" + this.props.book.id + "/chapters/1"}>
+            Goldilocks and the Three Bears...or Something Like That
+          </a>
+        </div>
       </div>
     )
   }
